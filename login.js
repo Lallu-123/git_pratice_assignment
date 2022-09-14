e7ec7db759e0d8039229158ef4210d8a1ddee3df
Your Name <lalith.lallu100@gmail.com>
2022-09-14 15:15:43 +0530

    make changes

---
+++ login.js
@@ -0,0 +1,18 @@
+//prime number;
+
+let num=17;
+let count=0;
+for(let i=1;i<=num;i++){
+if(num%i==0){
+count++
+}
+}
+if(count=1){
+console.log("yes")
+}else{
+console.log("no")
+}
+
+//palindrome
+let str="naman"
+for(let i=0;i<str.length;i++){
